8aadbba648d85d04e63ccc59cca00f6f794374dc
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2025-03-13 19:31:47 +0100

    cas 4

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -10,10 +10,30 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
+		if (!nomVendeurConnu) {
+			System.out.println("Je suis désolé " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
+		}
+		else {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal.");
+			boolean etalDisponible = controlPrendreEtal.resteEtals();
+			if (!etalDisponible) {
+				System.out.println("Désolé " + nomVendeur + ", je n'ai plus d'étal qui ne soit pas déjà occupé.");
+			}
+			else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est prafait, il me reste un étal pour vous !");
+		System.out.println("Il me faudrait quelques renseignements : ");
+		String produit = Clavier.entrerChaine("Quel produit souhaitez-vous vendre ?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if (numeroEtal != 1) {
+			System.out.println("Le vendeur " + nomVendeur + " s'est installé à l'étal n°" + numeroEtal + 1);
+		}
 	}
 }
