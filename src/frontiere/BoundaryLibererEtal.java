7c43aaaf049e5f3ac60d0e71144d7a1d9886868b
Sylvain Fraresso <sylvainfraresso@laposte.net>
2025-03-14 17:51:21 +0100

    cas 7

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -12,14 +12,14 @@ public class BoundaryLibererEtal {
 	public void libererEtal(String nomVendeur) {
 		boolean vendeurReconnu = controlLibererEtal.isVendeur(nomVendeur);
 		if (!vendeurReconnu) {
-			System.out.println("Mais vous n'êtes pas inscrit sur notre marché aujourd'hui !");
+			System.out.println("Mais vous n'Ãªtes pas inscrit sur notre marchï¿½ aujourd'hui !");
 		}
 		else {
 			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
 			boolean etalOccupe = donneesEtal[0].equals("true");
 			if (etalOccupe) {
 					System.out.println("Vous avez vendu " + donneesEtal[4] + " sur " + donneesEtal[3] + " " + donneesEtal[2]);
-					System.out.println("En revoir " + nomVendeur + ", passez une bonne journée.");
+					System.out.println("En revoir " + nomVendeur + ", passez une bonne journÃ©e.");
 			}
 		}
 	}
